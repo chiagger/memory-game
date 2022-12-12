05a4f3cef028a373895ec15ce3a6aa437ee91a4f
chiagger <busca.chiara.cb@gmail.com>
2022-12-12 23:29:33 +0100

    populated with img and sized all img correctly

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,17 +2,17 @@ import Card from './card';
 
 const Cards = () => {
     const cardsArray = [{ img: require('../img/alexvause.jpeg'), desc: 'Alex Vause' },
-    { img: '', desc: "Piper Chapman" },
-    { img: '', desc: "Nicky Nichols" },
-    { img: '', desc: "Poussey Washington" },
-    { img: '', desc: "Sophia Burset" },
-    { img: '', desc: "Suzanne Warren" },
-    { img: '', desc: "Tiffany Doggett" },
-    { img: '', desc: "Tasha Jefferson" },
-    { img: '', desc: "Galina Reznikov" },
-    { img: '', desc: "Maria Ruiz" },
-    { img: '', desc: "Big Boo" },
-    { img: '', desc: "Blanca Flores" }];
+    { img: require('../img/piperchapman.jpg'), desc: "Piper Chapman" },
+    { img: require('../img/nickynichols.jpg'), desc: "Nicky Nichols" },
+    { img: require('../img/pousseywashington.jpeg'), desc: "Poussey Washington" },
+    { img: require('../img/sophiaburset.jpg'), desc: "Sophia Burset" },
+    { img: require('../img/suzannewarren.webp'), desc: "Suzanne Warren" },
+    { img: require('../img/tiffanydoggett.webp'), desc: "Tiffany Doggett" },
+    { img: require('../img/tashajefferson.webp'), desc: "Tasha Jefferson" },
+    { img: require('../img/galinareznikov.webp'), desc: "Galina Reznikov" },
+    { img: require('../img/mariaruiz.webp'), desc: "Maria Ruiz" },
+    { img: require('../img/bigboo.webp'), desc: "Big Boo" },
+    { img: require('../img/blancaflores.webp'), desc: "Blanca Flores" }];
 
     return (
         <div id="cards">
